3a0daddfb2f9c62d74d9d61e5c9a856f8436a672
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-11-22 12:08:35 +0300

    add style to clock

--- TASK1/index.js
+++ TASK1/index.js
@@ -1,20 +1,37 @@
 function handleClock () {
-    const hourHand = document.getElementById("hour-hand");
-    const minuteHand = document.getElementById("minute-hand");
-    const secondHand = document.getElementById("second-hand");
+    // const hourHand = document.getElementById("hour-hand");
+    // const minuteHand = document.getElementById("minute-hand");
+    // const secondHand = document.getElementById("second-hand");
+
+    // const now = new Date();
+    // const hours = now.getHours() % 12; 
+    // const minutes = now.getMinutes();
+    // const seconds = now.getSeconds();
+
+    // const hourDeg = (hours + minutes / 60) * 360 / 12;
+    // const minuteDeg = (minutes + seconds / 60) * 360 / 60;
+    // const secondDeg = seconds * 360 / 60;
+
+    // hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    // minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    // secondHand.style.transform = `rotate(${secondDeg}deg)`;
 
     const now = new Date();
-    const hours = now.getHours() % 12; // Convert to 12-hour format
+    const hours = now.getHours();
     const minutes = now.getMinutes();
     const seconds = now.getSeconds();
 
-    const hourDeg = (hours + minutes / 60) * 360 / 12;
-    const minuteDeg = (minutes + seconds / 60) * 360 / 60;
-    const secondDeg = seconds * 360 / 60;
+    const hourHand = document.querySelector('.hour-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const secondHand = document.querySelector('.second-hand');
 
-    hourHand.style.transform = `rotate(${hourDeg}deg)`;
-    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
-    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    const hourRotation = (hours % 12 + minutes / 60) * 360 / 12;
+    const minuteRotation = (minutes + seconds / 60) * 360 / 60;
+    const secondRotation = seconds * 360 / 60;
+
+    hourHand.style.transform = `rotate(${hourRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+    secondHand.style.transform = `rotate(${secondRotation}deg)`;
 }
 setInterval(handleClock, 1000);
 handleClock()
